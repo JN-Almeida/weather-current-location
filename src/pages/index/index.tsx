291f2ae37f0307350ac220a1e64ecd9887abb136
Jonathan Almeida <jcalmeida.jonathan@gmail.com>
2022-04-12 12:26:51 -0300

    estruturing

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,12 @@
+import {Container} from './styles'
+
+const Index = () => {
+
+  return (
+    <Container>
+      Hello word
+    </Container>
+  )
+}
+
+export default Index;
---
+++ src/pages/index/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  max-width: 800px;
+  box-shadow: 0 0 0 #000;
+`
